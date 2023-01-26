8f949c8605163442527885b1024546e1889a38ba
EddyBautista <bautista.j.eddy@gmail.com>
2023-01-25 18:04:21 -0600

    ass styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import data from 'data.json'
 export default function Home() {
     return (
-      <div className="flex flex-col mx-auto w-full items-center mt-16 px-16">
+      <div className="flex flex-col mx-auto w-full items-center mt-16 px-16 ">
         <Image 
         className='rounded-full'
         alt={data.name}
@@ -11,7 +11,7 @@ export default function Home() {
         width={100}
         height={100}
         />
-        <h1 className="font-bold mt-4 mb-8 text-xl">{data.name}</h1>
+        <h1 className="font-bold mt-4 mb-8 text-xl text-white">{data.name}</h1>
         {data.links.map((link) => (
           <LinkCard key={link.href} {...link} />
         ))}
@@ -22,10 +22,11 @@ export default function Home() {
 
 const LinkCard = ({href, title}: {href:string; title:string}) => {
   return (
-    <a href={href} className='flex items-center w-full p-4 rounded-md 
-    hover:scale-105 transition-all border border-gray-300 mb-3'>
-      <div className="flex flex-col text-center">
-        <h2 className='font-semibold'>{title}</h2>
+    <a href={href} className='flex w-full p-4 rounded-md 
+    hover:scale-105 transition-all bg-white mb-3 '>
+      <div className="flex text-center w-full">
+        <div className='w-8 h-8'></div>
+        <h2 className='font-semibold text-grey-700'>{title}</h2>
       </div>
     </a>
   )
